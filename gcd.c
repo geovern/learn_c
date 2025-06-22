27a625a6905976e97c1f080bd04eb056ab4fc742
geovern <92625033+geovern@users.noreply.github.com>
2025-06-23 00:20:11 +0300

    gcd project

---
+++ gcd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int gcd(int a, int b) {
+	if(a % b == 0) {
+		return b;
+	} else {
+		return gcd(b, a % b);
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	printf("gcd(%s, %s) = %d",argv[1], argv[2], gcd(num1, num2));
+	
+	return 0;
+}
